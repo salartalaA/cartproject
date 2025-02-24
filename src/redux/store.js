2d245f5273d5b116547625c5629f414ddfb67c94
salartalaA <salarmhmdn@gmail.com>
2025-02-24 08:43:06 -0500

    fix: store using redux toolkit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
-import { createStore } from "redux";
-import rootReducer from "./rootReducer";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./rootReducer"; // Import your rootReducer here
 
-const store = createStore(
-  rootReducer,
-  {},
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = configureStore({
+  reducer: rootReducer, // Automatically combines reducers
+});
 
 export default store;
