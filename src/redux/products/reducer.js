2ba2490ca1393cd593b877f6d8d53863645e58da
salartalaA <salarmhmdn@gmail.com>
2025-02-24 08:44:57 -0500

    add: SET_FILTER case

--- src/redux/products/reducer.js
+++ src/redux/products/reducer.js
@@ -1,16 +1,24 @@
-import { SET_PRODUCTS } from "./actionType";
+import { SET_FILTER, SET_PRODUCTS } from "./actionType";
 
-const intialState = {
+const initialState = {
   products: [],
+  filter: "",
 };
 
-const productReducer = (state = intialState, action) => {
+const productReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_PRODUCTS:
       return {
         ...state,
         products: action.payload,
       };
+
+    case SET_FILTER: {
+      return {
+        ...state,
+        filter: action.payload,
+      };
+    }
 
     default:
       return {
